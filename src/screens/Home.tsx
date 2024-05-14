a56076b2f25e3a1e9a864f2ff1932a9b20189b61
Gelson <gelsondeveloper@hotmail.com>
2024-05-14 19:01:10 -0300

    -frist commit

---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+
+export function Home() {
+    return (
+
+        <View className="flex-1 items-center justify-center bg-black">
+            <Text className='text-3xl text-white font-bold'>NativeWind</Text>
+        </View>
+    )
+
+}
